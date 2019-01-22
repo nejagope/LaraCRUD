00a9872f1aac1df181a28a05f5ac24b4547dc9cd
Nelson González <njgp86@gmail.com>
2019-01-21 20:13:53 -0600

    Added edit template to laravel views

---
+++ app/laravel/views/edit.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.main')
+
+@section('head')
+@stop
+
+@section('content')
+
+<div class="container">
+    <div class="row">        
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">                                
+            
+                <div class="panel-heading">                    
+                    <h4>Edit %TableSingular%</h4><hr>
+                </div>
+                <div class="panel-body">                   					
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('%RoutePrefix%_%Table%_udate', ['%TableSingular%' => $%TableSingular%]) }}">
+                        {{ csrf_field() }}
+						
+                        %Inputs%
+                       
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Update
+                                </button>
+								<a class="btn btn-primary" href="#" onclick="window.history.back()"> 
+									Cancel
+								</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+@stop
