a5321c968fe4a774f5b75c7a65d54c1bf9a56a68
Nelson González <njgp86@gmail.com>
2019-01-26 23:02:50 -0600

    Corrected index view template

--- app/laravel/views/index.blade.php
+++ app/laravel/views/index.blade.php
@@ -1,49 +1,61 @@
 @extends('layouts.main')
 
 @section('head')
+<script>
+	function confirmation(msj){
+		return confirm(msj);
+	}
+</script>
 @endsection
 
 @section('content')
 
 <div class="container">
     <div class="row">        
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">                                
+        <div class="col-md-12">
             
-                <div class="panel-heading">
-					<h4> %Table% </h4>
-					<hr>
-                </div>
+			<h4> %Table% </h4>
+			<hr>
+			<table class="table table-striped table-responsive">
+				<tr>
+					%ColumnHeaders%
+					<th> 
+						<a href="{{ route('%RoutePrefix%_%Table%_create') }}"> 
+							<span class="fa fa-plus-circle"></span> Agregar
+						</a>
+					</th>
+				</tr>
+				@foreach ($%Table% as $%TableSingular%)                                                                   
+					<tr>
+						%Fields%																                                        								
+						<td>
+							<table>
+								<tr>
+									<td>
+										<a class="btn btn-warning btn-sm" href="{{ route('%RoutePrefix%_%Table%_edit', ['%TableSingular%' => $%TableSingular%]) }}"> 
+											Edit
+										</a>
+									</td>
 
-                <div class="panel-body">
-                    <table class="table table-striped table-responsive">
-						<tr>
-							%ColumnHeaders%
-							<th> 
-								<a href="{{ route('%RoutePrefix%_%Table%_create') }}"> 
-								   <span class="fa fa-plus-circle"></span> Agregar
-								</a>
-							</th>
-						</tr>
-						@foreach ($%Table% as $%TableSingular%)                                                                   
-							<tr>
-								%Fields%																                                        								
-								<td width="40%">
-									<a class="btn btn-warning btn-sm" href="{{ route('%RoutePrefix%_%Table%_edit', ['%TableSingular%' => $%TableSingular%]) }}"> 
-										Edit
-									</a>
+									<td>
+										<form class="form form-inline" method="post" action="{{ route('%RoutePrefix%_%Table%_restore', ['%TableSingular%' => $%TableSingular%]) }}" onsubmit="return confirmation('Confirm restauration: ' + {{$%TableSingular%->id}});">								
+											{{ csrf_field() }}										
+											<input type="submit" class="btn btn-success btn-sm"  value="Restore">																				
+										</form>  
+									</td>
 
-									<form class="form form-inline" method="post" action="{{ route('%RoutePrefix%_%Table%_delete', ['%TableSingular%' => $%TableSingular%]) }}" onsubmit="return confirmDelete();">								
-										 {{ csrf_field() }}										
-										<input type="submit" class="btn btn-danger btn-sm"  value="Delete">																				
-									</form>                                        
-								</td>
-							</tr>                                    
-						@endforeach                            
-					</table>                                                 					
-                </div>
-				
-            </div>
+									<td>
+										<form class="form form-inline" method="post" action="{{ route('%RoutePrefix%_%Table%_delete', ['%TableSingular%' => $%TableSingular%]) }}" onsubmit="return confirmation('Confirm deletion: ' + {{$%TableSingular%->id}});">								
+											{{ csrf_field() }}										
+											<input type="submit" class="btn btn-danger btn-sm"  value="Delete">																				
+										</form>   
+									</td>  
+								</tr>
+							</table>
+						</td>
+					</tr>                                    
+				@endforeach                            
+			</table>                                                 					                
         </div>
     </div>
 
